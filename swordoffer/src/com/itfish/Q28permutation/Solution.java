32ed30e2e76cf2fc096d028fb963b413d8426dee
张渝 <zhangyu@MacBook-Air.local>
2017-05-09 22:39:33 +0800

    save

--- swordoffer/src/com/itfish/Q28permutation/Solution.java
+++ swordoffer/src/com/itfish/Q28permutation/Solution.java
@@ -1,7 +1,17 @@
 package com.itfish.Q28permutation;
 
+import java.util.ArrayList;
+
 /**
  * Created by zhangyu on 16/8/31.
  */
 public class Solution {
+    public ArrayList<String> Permutation(String s){
+
+
+
+
+
+    }
+    public
 }
